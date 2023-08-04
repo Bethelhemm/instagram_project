cb806681630d51f5dbb6bf68b2d6fddeefa5a3b8
Bethelhemm <122419774+Bethelhemm@users.noreply.github.com>
2023-08-04 16:36:47 +0300

    Update main.dart

--- instagram/lib/main.dart
+++ instagram/lib/main.dart
@@ -6,18 +6,108 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Instagram',
       theme: ThemeData(
+        iconTheme: IconThemeData(color: Color.fromRGBO(40, 40, 40, 1)),
+        appBarTheme: AppBarTheme(
+          elevation: 1,
+          color: Colors.grey[50],
+          iconTheme: IconThemeData(color: Color.fromRGBO(40, 40, 40, 1)),
+        ),
         primaryColor: Colors.white,
         visualDensity: VisualDensity.adaptivePlatformDensity,
         useMaterial3: true,
       ),
-      home: const BottomNavBar(),
+      home: MyHomePage(),
     );
   }
 }
+
+class MyhomePage extends StatefulWidget {
+  const MyhomePage({super.key});
+
+  @override
+  State<MyhomePage> createState() => _MyhomePageState();
+}
+
+class _MyhomePageState extends State<MyhomePage> {
+  int currentPage = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: currentPage == 1 ? SearchPage() : HomePage(),
+      bottomNavigationBar: BottomAppBar(
+        child: Row(
+          children: [
+            IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.home,
+                  color: currentPage == 0
+                      ? Color.fromRGBO(203, 73, 101, 1)
+                      : Color.fromRGBO(40, 40, 40, 1),
+                )),
+            Spacer(),
+            IconButton(
+                onPressed: () {
+                  setState(() {
+                    currentPage = 1;
+                  });
+                },
+                icon: Icon(
+                  Icons.search,
+                  color: currentPage == 1
+                      ? Color.fromRGBO(203, 73, 101, 1)
+                      : Color.fromRGBO(40, 40, 40, 1),
+                )),
+            Spacer(),
+            IconButton(
+                onPressed: () {
+                  setState(() {
+                    currentPage = 0;
+                  });
+                },
+                icon: Icon(
+                  Icons.add_box_rounded,
+                  color: currentPage == 0
+                      ? Color.fromRGBO(5, 2, 3, 1)
+                      : Color.fromRGBO(40, 40, 40, 1),
+                )),
+            Spacer(),
+            IconButton(
+                onPressed: () {
+                  setState(() {
+                    currentPage = 0;
+                  });
+                },
+                icon: Icon(
+                  Icons.ondemand_video,
+                  color: currentPage == 0
+                      ? Color.fromRGBO(203, 73, 101, 1)
+                      : Color.fromRGBO(40, 40, 40, 1),
+                )),
+            Spacer(),
+            IconButton(
+                onPressed: () {
+                  setState(() {
+                    currentPage = 0;
+                  });
+                },
+                icon: Icon(
+                  Icons.person,
+                  color: currentPage == 0
+                      ? Color.fromRGBO(203, 73, 101, 1)
+                      : Color.fromRGBO(40, 40, 40, 1),
+                )),
+            Spacer(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
